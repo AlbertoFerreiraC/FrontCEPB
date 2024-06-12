b1bfc487c5226e6b4790970ae7631405a0528e83
jeisol <jeisol@jeisol.myguest.virtualbox.org>
2024-06-11 22:33:29 -0400

    cambios de prueba

---
+++ CEPB/src/app/arancel/arancel.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arancel',
+  templateUrl: './arancel.component.html',
+  styleUrl: './arancel.component.css'
+})
+export class ArancelComponent {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
